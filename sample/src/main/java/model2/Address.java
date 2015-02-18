563481c9b70ddad930eaaee7ee84e2e878ef19f8
Felipe Lima <felipe.lima@gmail.com>
2015-02-18 12:19:00 -0800

    Adds builder usage example

--- sample/src/main/java/model2/Address.java
+++ sample/src/main/java/model2/Address.java
@@ -1,14 +1,28 @@
 package model2;
 
-import auto.parcel.AutoParcel;
 import android.os.Parcelable;
+
+import auto.parcel.AutoParcel;
 
 @AutoParcel
 public abstract class Address implements Parcelable {
   public abstract double[] coordinates();
+
   public abstract String cityName();
 
+  @AutoParcel.Builder interface Builder {
+    Builder coordinates(double[] c);
+
+    Builder cityName(String c);
+
+    Address build();
+  }
+
+  static Builder builder() {
+    return new AutoParcel_Address.Builder();
+  }
+
   public static Address create(double[] coordinates, String cityName) {
-    return new AutoParcel_Address(coordinates, cityName);
+    return builder().coordinates(coordinates).cityName(cityName).build();
   }
 }
